bbf816342677785811d5ef8ded939faf76ea3d08
nathan.silva <nathan.silva@moochies.com>
2024-03-31 19:20:35 -0300

    feat: add unit-test for Vehicle Resource

--- src/test/java/br/com/cleanarch/app/rest/VehicleResourceTest.java
+++ src/test/java/br/com/cleanarch/app/rest/VehicleResourceTest.java
@@ -2,9 +2,11 @@ package br.com.cleanarch.app.rest;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.not;
 
 @QuarkusTest
 class VehicleResourceTest {
@@ -14,7 +16,13 @@ class VehicleResourceTest {
     }
 
     @Test
-    void findAllVehicles() {
+    @DisplayName("Should get all vehicles registered in database")
+    void shouldFindAllVehicles() {
+        given()
+                .when().get("/api/v1/veiculos")
+                .then()
+                .statusCode(200)
+                .body("size()", not(0));
     }
 
     @Test
